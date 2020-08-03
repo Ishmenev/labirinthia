13a64c3999d90fb341445b703bb8229b703cad79
Ishmenev <ishmenevev@mail.ru>
2020-08-03 18:39:32 +0300

    fixed all

---
+++ client/src/setupProxy.js
@@ -0,0 +1,9 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+module.exports = function (app) {
+  app.use(
+    ["/api", "/auth/google", "/auth/vkontakte", "/auth/facebook"],
+    createProxyMiddleware({
+      target: "http://localhost:80",
+    })
+  );
+};
---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID: '490964350427-eiirerq5h16c7s76jq4e9u485rnuaphf.apps.googleusercontent.com',
+  googleClientSecret: 'KxwRGcrOfFpnL51IYbjoe9N6',
+  mongoURI: 'mongodb+srv://EvgenyLabirintov:labirinthia-23@cluster0-qzdkt.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  cookieKey: 'hjfhdjflsdklskdlkdlwkdl',
+  vkClientID: '7492824',
+  vkClientSecret: 'zq1pCzqihumkMjYpHKJC',
+  facebookClientID: '255767969026240',
+  facebookClientSecret: '4fb9bd7fafbbe2ad700f9d562aeaad93'
+}
